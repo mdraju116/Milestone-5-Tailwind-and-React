2f4e91baabe0fd0d731a12d85b15ee0b6200c503
Raju204116 <raju204116@gmail.com>
2026-09-11 00:04:28 +0600

    deployed

---
+++ Module-29-React-Project-Countries-Explorer/c29-9-deploy-using-netlify-and-vercel.jsx
@@ -0,0 +1,47 @@
+
+/* 
+✅✅1.Netlify.com
+
+=>In the vscode terminal-project directory:
+    -type : npm run build  (a dist folder will create automatically)
+
+=>Go to netlify.com
+-login with google
+-go to projects 
+-Scroll down to : 
+    Production deploys
+        Drag and drop your project folder here to deploy new changes.
+        Or browse files to uploadNo file chosen or choose a folder 
+-click on choose a folder 
+-select the dist folder inside the project
+-Upload and make it public
+
+📌link : https://ephemeral-rabanadas-388c7c.netlify.app/
+
+⚠️NB: After any change of the code, we need to build the project again and
+-drag and drop the folder again in the same deployment folder
+
+
+⚠️=>We can also do it by connecting github account
+-selecting diretly ropsitoty.
+
+
+✅✅2.Vercel.app
+
+=>In the vscode terminal-project directory:
+    -type : npm run build  (a dist folder will create automatically)
+
+=>login vercel 
+-Click to Add New -project
+-Scroll down to : You can also drag and drop your project, or choose a file or a folder.
+-click choose a file or a folder.
+-select the dist folder inside the project
+-Upload and make it public
+
+📌link : https://country-explorer-six-xi.vercel.app/
+
+⚠️=>We can also do it by connecting github account
+-selecting diretly ropsitoty.
+
+
+*/
