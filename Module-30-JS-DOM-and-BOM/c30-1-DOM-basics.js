f877ba95e400ff8125ba6fe10255a7960a1107f5
Raju204116 <raju204116@gmail.com>
2026-09-11 14:24:11 +0600

    c30-2

--- Module-30-JS-DOM-and-BOM/c30-1-DOM-basics.js
+++ Module-30-JS-DOM-and-BOM/c30-1-DOM-basics.js
@@ -1,6 +1,13 @@
 /* 
 ✅ ➡️ 
 
+1.What is DOM ?
+2.How does browser create DOM ?
+3.Tree structure of HTML code 
+4.Different types of nodes in DOM
+5.Real life analogy
+6.Why we need to understand DOM?
+
 
 
 */
