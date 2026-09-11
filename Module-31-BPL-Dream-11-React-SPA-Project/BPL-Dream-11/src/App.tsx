cf5a3be5fdacb85703425c6dbb9d2a4642907d7f
Raju204116 <raju204116@gmail.com>
2026-09-11 20:10:19 +0600

    BPL-Dream-11-project-created

---
+++ Module-31-BPL-Dream-11-React-SPA-Project/BPL-Dream-11/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      BPL Dream-11
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
